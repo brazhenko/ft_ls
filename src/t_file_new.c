9fc60cbb970eae22bb7a44dd49f8db0c796e7f7c
ShnurD6/bbaelor- <shnurd6@mail.ru>
2019-01-18 21:26:17 +0300

    test push

--- src/t_file_new.c
+++ src/t_file_new.c
@@ -1,33 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ls_dir.c                                           :+:      :+:    :+:   */
+/*   t_file_new.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbaelor- <bbaelor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/18 20:40:28 by lreznak-          #+#    #+#             */
-/*   Updated: 2019/01/18 21:24:42 by bbaelor-         ###   ########.fr       */
+/*   Created: 2019/01/18 21:19:18 by bbaelor-          #+#    #+#             */
+/*   Updated: 2019/01/18 21:21:41 by bbaelor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-t_file			*ls_dir(DIR *cur_dir)
+t_file  *t_file_new(char *name)
 {
-	struct dirent	*file;
-	t_file			*file_lst = NULL;
+    t_file *new;
 
-	while ((file = readdir(cur_dir)))
-	{
-		t_file *node = t_file_new(file->d_name);
-		node->prev = file_lst;
-		if (file_lst)
-			file_lst->next = node;
-		file_lst = node;
-		printf("write.. %s\n", file->d_name);
-		
-	}
-
-
-	return (NULL);	
+    new = (t_file *)malloc(sizeof(t_file));
+    new->name = name;
+    new->prev = NULL;
+    new->next = NULL;
+    return (new);
 }
