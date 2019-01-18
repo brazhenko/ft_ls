7ea4a540e030e82367d65a41bd42509c6c2663ca
ShnurD6/bbaelor- <shnurd6@mail.ru>
2019-01-18 22:17:26 +0300

    stat in t_file_new

--- src/t_file_new.c
+++ src/t_file_new.c
@@ -6,7 +6,7 @@
 /*   By: bbaelor- <bbaelor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 21:19:18 by bbaelor-          #+#    #+#             */
-/*   Updated: 2019/01/18 22:08:49 by lreznak-         ###   ########.fr       */
+/*   Updated: 2019/01/18 22:16:00 by bbaelor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ t_file  *t_file_new(char *name, char *full_name)
     new->name = name;
     new->prev = NULL;
     new->next = NULL;
+    stat(full_name, new->dir_stat);
+
     return (new);
 }
