e56ba683ef1711164753e3dc1c75756f0ee89f2c
Lommy greenhands Reznak mo reznak <andreybrazhenko@yandex.ru>
2019-01-19 02:39:45 +0300

    main

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: bbaelor- <bbaelor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 11:33:06 by bbaelor-          #+#    #+#             */
-/*   Updated: 2019/01/19 01:05:54 by lreznak-         ###   ########.fr       */
+/*   Updated: 2019/01/19 02:39:28 by lreznak-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,8 @@
 
 int		main(int c, char **v)
 {
-	t_flags *flags = malloc(sizeof(t_flags));
-	int last_flag = parser(c, v, flags);
-	printf("%d\n\n", last_flag);
-	printf("%d\n%d\n%d\n%d\n",
-			flags->a,
-			flags->l,
-			flags->rec,
-			flags->t);
 
-	if (last_flag == c || last_flag + 1 == c)
-	{
-		if (flags->rec)
-			ls_files_r(".");
-		else
-			ls_dir(opendir("."), ".");
-	}
-	else
-	{
-		while (last_flag < c)
-		{
-				
-		}
-	}
-	
+
 	// ls_files_r(argv[1]);
 	return (0);
 }
