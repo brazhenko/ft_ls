41df902065099948c19818b1215a9f1dd1a8a350
Lommy greenhands Reznak mo reznak <andreybrazhenko@yandex.ru>
2019-01-19 03:34:14 +0300

    main

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: bbaelor- <bbaelor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 11:33:06 by bbaelor-          #+#    #+#             */
-/*   Updated: 2019/01/19 03:18:35 by lreznak-         ###   ########.fr       */
+/*   Updated: 2019/01/19 03:34:04 by lreznak-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,22 @@ int		main(int c, char **v)
 {
 	char			*flags;
 	int				last_flag;
-	flags = parse_flags(c, v, &last_flag);
 
-	printf("%d\n", last_flag);
-	// ls_files_r(argv[1]);
+	flags = parse_flags(c, v, &last_flag);
+	if (last_flag++)
+	{
+		while (last_flag < c)
+		{
+			
+			last_flag++;
+		}
+	}
+	else
+	{
+		if (flags['R'])
+			ls_files_r(".");
+		else
+			ls_dir(opendir("."), ".");
+	}
 	return (0);
 }
--- src/ls_dir.c
+++ src/ls_dir.c
@@ -6,7 +6,7 @@
 /*   By: bbaelor- <bbaelor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 20:40:28 by lreznak-          #+#    #+#             */
-/*   Updated: 2019/01/19 01:05:56 by lreznak-         ###   ########.fr       */
+/*   Updated: 2019/01/19 03:30:24 by lreznak-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ t_file			*ls_dir(DIR *cur_dir, char *full_name)
 	}
 	while (file_lst)
 	{
-		printf("! %s\n", file_lst->name);
+		printf("%s\n", file_lst->name);
 		// ft_putendl(file_lst->name);
 		file_lst = file_lst->prev;
 	}
