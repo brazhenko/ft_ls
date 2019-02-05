ecf2921a99a4f0ff81a6d95e34ef257daa600133
lreznak- <andreybrazhenko@yandex.ru>
2019-02-05 06:29:20 +0300

    mode print modified

--- src/print_mode.c
+++ src/print_mode.c
@@ -13,104 +13,21 @@ static void bin( int k )
 
 void 		print_mode(int mode)
 {
+    printf("%c", (S_ISDIR(mode)) ? 'd' : '-');
+	printf("%c", (mode & 256) ? 'r' : '-');
+    printf("%c", (mode & 128) ? 'w' : '-');
+    printf("%c", (mode & 64) ? 'x' : '-');
 
+    printf("%c", (mode & 32) ? 'r' : '-');
+    printf("%c", (mode & 16) ? 'w' : '-');
+    printf("%c", (mode & 8) ? 'x' : '-');
 
-	// printf("--%d--", !(mode & 256 / 256));
-//	ft_putchar('r' * (mode & 256 / 256) + '-' * !(mode & 256 / 256));
-//	ft_putchar('w' * (mode & 128 / 128) + '-' * !(mode & 128 / 128));
-//	ft_putchar('x' * (mode & 64 / 64) + '-' * !(mode & 64 / 64));
-//	ft_putchar('r' * (mode & 32 / 32) + '-' * !(mode & 32 / 32));
-//	ft_putchar('w' * (mode & 16 / 16) + '-' * !(mode & 16 / 16));
-//	ft_putchar('x' * (mode & 8 / 8) + '-' * !(mode & 8 / 8));
-//	ft_putchar('r' * (mode & 4 / 4) + '-' * !(mode & 4 / 4));
-//	ft_putchar('w' * (mode & 2 / 2) + '-' * !(mode & 2 / 2));
-//	ft_putchar('x' * (mode & 1 / 1) + '-' * !(mode & 1 / 1));
-//	bin(mode);
-//	puts("            ");
-
-// извини, у нас пиздец с буфером((
-	
-	// if (S_ISDIR(mode))
-	// 	write(1, "d", 1);
-	// else
-	// 	write(1, "-", 1);
-	// if (mode & 256)
-	// 	write(1, "r", 1);
-	// else
-	// 	write(1, "-", 1);
-	// if (mode & 128)
-	// 	write(1, "w", 1);
-
-	// else
-	// 	write(1, "-", 1);
-	// if (mode & 64)
-	// 	write(1, "x", 1);
-	// else
-	// 	write(1, "-", 1);
-	// if (mode & 32)
-	// 	write(1, "r", 1);
-	// else
-	// 	write(1, "-", 1);
-	// if (mode & 16)
-	// 	write(1, "w", 1);
-	// else
-	// 	write(1, "-", 1);
-	// if (mode & 8)
-	// 	write(1, "x", 1);
-	// else
-	// 	write(1, "-", 1);
-	// if (mode & 4)
-	// 	write(1, "r", 1);
-	// else
-	// 	write(1, "-", 1);
-	// if (mode & 2)
-	// 	write(1, "w", 1);
-	// else
-	// 	write(1, "-", 1);
-	// if (mode & 1)
-	// 	write(1, "x", 1);
-	// else
-	// 	write(1, "-", 1);
-
-	if (S_ISDIR(mode))
-		printf("d");
-	else
-		printf("-");
-	if (mode & 256)
-		printf("r");
-	else
-		printf("-");
-	if (mode & 128)
-		printf("w");
-
-	else
-		printf("-");
-	if (mode & 64)
-		printf("x");
-	else
-		printf("-");
-	if (mode & 32)
-		printf("r");
-	else
-		printf("-");
-	if (mode & 16)
-		printf("w");
-	else
-		printf("-");
-	if (mode & 8)
-		printf("x");
-	else
-		printf("-");
-	if (mode & 4)
-		printf("r");
-	else
-		printf("-");
-	if (mode & 2)
-		printf("w");
-	else
-		printf("-");
-	if (mode & 1)
-		printf("x");
-	else
-		printf("-");
+    printf("%c", (mode & 4) ? 'r' : '-');
+    printf("%c", (mode & 2) ? 'w' : '-');
+    if (mode & 1)
+        printf("x");
+    else if (mode & 512)
+        printf("T");
+    else
+        printf("-");
 }
