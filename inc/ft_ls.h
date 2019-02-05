e0f1d9efa196d7502ab0865b7613dbd02ef38255
lreznak- <andreybrazhenko@yandex.ru>
2019-02-05 09:30:44 +0300

    header norm

--- inc/ft_ls.h
+++ inc/ft_ls.h
@@ -27,7 +27,7 @@
 # include <time.h>
 # include <fcntl.h>
 
-typedef struct				s_all
+typedef struct		s_all
 {
 	char					*flags;
 	int						len_cs;
@@ -35,9 +35,9 @@ typedef struct				s_all
 	int						len_gr;
 	int						len_ves;
 	int						len_namef;
-}							t_all;
+}					t_all;
 
-typedef struct				s_file
+typedef struct		s_file
 {
 	struct s_file	*next;
 	struct s_file	*prev;
@@ -51,9 +51,7 @@ typedef struct				s_file
 	int				blocks_a;
 	int				blocks;
 	int				is_exist;
-}							t_file;
-
-
+}					t_file;
 
 int					ls_files_r(char *name, t_all *all);
 t_file				*ls_dir(DIR *cur_dir, char *name, t_all *all);
@@ -65,10 +63,12 @@ void				add_to_lst(t_file **lst, t_file *node);
 t_file				*to_list(struct dirent *file, char *name, 		\
 					char *full_name, t_file *list);
 void				print_mode(int mode);
-struct				s_file *merge_sort(struct s_file *head, int (*f)(t_file *, t_file *));
-int					insert(struct s_file **head, char *name, char *full_name, t_all *all);
+struct s_file		*merge_sort(struct s_file *head,
+								int (*f)(t_file *, t_file *));
+int					insert(struct s_file **head, char *name,
+										char *full_name, t_all *all);
 int					comparator_classic(t_file *left, t_file *right);
-void                t_file_del(t_file *node);
+void				t_file_del(t_file *node);
 t_file				*config_compare(t_file	*file_lst, t_all *all);
 void				ls_only_file(t_file *file_lst, t_all *all);
 void				printf_len_hu_num(unsigned short int num, int n);
@@ -76,7 +76,8 @@ void				prt_st(char *str, int n);
 void				printf_len_llnum(long long int num, int n);
 char				*cut_time(char *str);
 void				printf_len_post_str(char *str, int n);
-int					insert(struct s_file **head, char *name, char *full_name, t_all *all);
+int					insert(struct s_file **head, char *name,
+									char *full_name, t_all *all);
 void				insert_file(t_all *all);
 int					comparator_classic(t_file *left, t_file *right);
 int					comparator_r(t_file *left, t_file *right);
